699909bf7091ab93dcad42dffdbdd41c64f9640a
Zen-Hub-Exploits <85144919+Zen-Hub-Exploits@users.noreply.github.com>
2021-05-31 20:40:21 -0600

    Update and rename AztupBrew Constant Dumper.lua to Source.lua

--- AztupBrew Constant Dumper.lua
+++
@@ -1,9 +0,0 @@
---AztupBrew constant dumper
---Made by Superscripted#0201
---Join Discord discord.gg/7gD9dX2gyS
---Run this in https://replit.com/languages/lua
-local old=table.concat
-table.concat=function(brr)print(old(brr))return old(brr)end
---Script goes below v v
-
---Script goes above ^ ^
---
+++ Source.lua
@@ -0,0 +1,4 @@
+--Zen Hub
+--Created by Superscripted#0201
+--Created by Xen#5621
+--Join Discord discord.gg/7gD9dX2gyS
